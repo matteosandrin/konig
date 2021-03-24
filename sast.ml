479f5461399eac52190bded7c1eb2ec4dbd06f22
Matteo Sandrin <matteo.sandrin@hotmail.it>
2021-03-24 14:59:05 -0400

    Create sast.ml

---
+++ sast.ml
@@ -0,0 +1,37 @@
+(* Abstract Syntax Tree *)
+open Ast
+
+type sexpr = typ * sx
+type sx =
+    SLiteral of int
+  | SFliteral of string
+  | SBoolLit of bool
+  | SStrLit of string
+  | SListLit of sexpr list
+  | SNodeLit of sexpr list
+  | GraphLit of sexpr list
+  | SId of string
+  | SBinop of sexpr * op * sexpr
+  | SUnop of uop * sexpr
+  | SAssign of string * sexpr
+  | SCall of string * sexpr list
+  | SIndex of string * sexpr (* access an item in an array *)
+  | SNoexpr
+
+type sstmt =
+    SBlock of sstmt list
+  | SExpr of sexpr
+  | SReturn of sexpr
+  | SIf of sexpr * sstmt * sstmt
+  | SFor of sexpr * sexpr * sexpr * sstmt
+  | SWhile of sexpr * sstmt
+
+type sfunc_decl = {
+    styp : typ;
+    sfname : string;
+    sformals : bind list;
+    slocals : bind list;
+    sbody : sstmt list;
+  }
+
+type sprogram = bind list * sfunc_decl list
